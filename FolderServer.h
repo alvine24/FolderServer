f316176686b8e5b31ab60fc340f86ea7b84a8ce6
Alvine AGBO <alvine.agbo@gmail.com>
2013-06-07 18:05:46 +0800

    Comments

--- FolderServer.h
+++ FolderServer.h
@@ -1,3 +1,14 @@
+/**
+ * @file FolderServer.h
+ * @brief The FolderServer class will contain all required
+ * definitions and basic utilities functions for the synchronization
+ * betweeen a client folder and the server or remote
+ *
+ * @author Alvine AGBO
+ * @date 6/07/2013
+ * @version 1.0
+ */
+
 #ifndef FOLDERSERVER_H
 #define FOLDERSERVER_H
 
@@ -13,6 +24,11 @@ class FolderServer : public QObject {
     Q_OBJECT
 
 public:
+    /**
+     * @brief FolderServer
+     * Constructor that set the connection to the server
+     * This uses SheerCloudLink class to perform all the function
+     */
     FolderServer(): link("http://localhost:8080", "abc", "123")
     {
         link.connect( &link, SIGNAL(done()), &loop, SLOT(quit()));
@@ -31,8 +47,28 @@ public:
     int m_progress_reports;
 
 public slots:
+    /**
+     * @brief loginLink
+     * This slot helps to connect to the server
+     */
     void loginLink();
+
+    /**
+     * @brief uploadFolder
+     * This slot helps to send all files and directories from
+     * a local folder to the server. The folders' structure is
+     * the same at both sides.
+     * @param myFolder is the path of the main folder at the client side
+     * @param serverPath is the path of the folder at the server side
+     */
     void uploadFolder(QString myFolder, QString serverPath);
+
+    /**
+     * @brief progress_check
+     * This slot helps to check the progress of the uploading process
+     * @param now
+     * @param total
+     */
     void progress_check(qint64 now, qint64 total);
 
 };
